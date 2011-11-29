361c139aa87172d6b459626c2245aa0b68d9e3f7
orrigautur <orrigautur@gmail.com>
2011-11-29 15:40:12 +0000

    removing more files

--- public/javascripts/snarpur/app/view/timeline/itemLine.js
+++
@@ -1,8 +0,0 @@
-sn.declare("Timeline.ItemLine.view.point","\
-    <div class='item_line ${name}'></div>\
-");
-
-sn.declare("Timeline.ItemLine.view.period","\
-    <div class='item_line'><p><em>${_s.capitalize(name)}</em></p></div>\
-");
-
