3ae012c2836143d92ff4188b48de000ae825e03f
orrigautur <orrigautur@gmail.com>
2011-12-09 22:10:26 +0000

    revert back to locale :is

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Snarpur
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :is
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
     # config.generators do |g|
