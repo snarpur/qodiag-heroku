b64613211afc4e62102ac71cded1b3fc59f27ac2
Qodiag <ataman@gmail.com>
2014-03-12 15:24:48 +0000

    Adding info messages to  after_migrate.rb

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,7 +1,7 @@
- puts "working in environment #{config.environment_name}"
+info("working in environment #{config.environment_name}")
 if config.environment_name == "alfa"
-  puts "reseting the database...."
+  info("reseting the database....")
   run "bundle exec rake db:populate_reset[reset] >> #{config.release_path}/log/populate_reset.log"
-  puts "populating data to the database...."
+  info("populating data to the database....")
   run "bundle exec rake db:populate_ini_file[orri] >> #{config.release_path}/log/populate.log"
 end
