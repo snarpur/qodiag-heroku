3473aac92a831108020db59a28239f169b05ba46
Qodiag <ataman@gmail.com>
2014-03-12 14:54:40 +0000

    Fix error in after_migrate.rb

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,7 +1,7 @@
-run "echo 'working in environment #{config.environment_name}'"
+info "working in environment #{config.environment_name}"
 if config.environment_name == "alfa"
-  run "echo 'reseting the database....'"
-  run "bundle exec rake db:populate_reset[reset] >> #{release_path}/log/populate_reset.log"
-  run "echo 'populating data to the database....'"
-  run "bundle exec rake db:populate_ini_file[orri] >> #{release_path}/log/populate.log"
+  info "reseting the database...."
+  run "bundle exec rake db:populate_reset[reset] >> #{config.release_path}/log/populate_reset.log"
+  info "populating data to the database...."
+  run "bundle exec rake db:populate_ini_file[orri] >> #{config.release_path}/log/populate.log"
 end
