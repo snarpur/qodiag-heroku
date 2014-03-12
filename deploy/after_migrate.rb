85c00a59e6a369c78d9f07d5d5b5ec6bbf637fe6
Qodiag <ataman@gmail.com>
2014-03-12 15:50:33 +0000

    Final deployment with deploy hooks

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,7 +1,4 @@
-info("working in environment #{config.environment_name}")
 if config.environment_name == "alfa"
-  info("reseting the database....")
   run "bundle exec rake db:populate_reset[reset] >> #{config.release_path}/log/populate_reset.log"
-  info("populating data to the database....")
   run "bundle exec rake db:populate_ini_file[orri] >> #{config.release_path}/log/populate.log"
 end
